a8deb087239e34e3cb7b8a02c9aedaf65bcee282
Muhammad Maulana Hikam <maulanahikam085@gmail.com>
2022-12-14 19:26:44 +0700

    Program Update v0.2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include "pustaka.h"
 
-int main(){
-
-    string listLagu[10] = {"Scared",
+int main()
+{
+    int dataList = 20;
+    string listLagu[dataList] = {"Scared",
                            "All the kids are depressed",
                            "Daylight",
                            "18",
@@ -11,57 +12,96 @@ int main(){
                            "Circles",
                            "Sunflower"};
 
-    int durasiLagu[10] = {3, 4, 4, 4, 3, 2, 3, 2};
-    string listGenre[10] = {"POP", "POP", "POP", "POP", 
+    int durasiLagu[dataList] = {3, 4, 4, 4, 3, 2, 3, 2};
+    string listGenre[dataList] = {"POP", "POP", "POP", "POP",
                             "POP", "POP", "POP", "POP"};
-
     int choose;
+    for (i = 0; i < 8; i++)
+    {
+        insertEnd(listLagu[i], durasiLagu[i], listGenre[i]);
+    }
+
     do
     {
+        system("cls");
         cout << endl;
         cout << " =============================" << endl;
-        cout << " =     PROGRAM LIST MUSIC    =" << endl;
+        cout << " =    PROGRAM LIST MUSIC     =" << endl;
         cout << " =============================" << endl;
         cout << " = 1. Insert Music           =" << endl;
-        cout << " = 2. Show Music             =" << endl;
-        cout << " = 3. Exit Program           =" << endl;
+        cout << " = 2. Delete Music           =" << endl;
+        cout << " = 3. Show Music             =" << endl;
+        cout << " = 4. Exit Program           =" << endl;
         cout << " =============================" << endl;
         cout << " Pilihan : ";
         cin >> choose;
 
-        switch (choose){
-            case 1 : system("cls");{
-                cout << "Masukkan nama game : ";
-                cin.ignore();
-                getline(cin, namaGame);
-                cout << "Masukkan tahun rilis game : ";
-                cin >> tahun;
-                insertData(namaGame, tahun);
+        switch (choose)
+        {
+        case 1:
+            system("cls");
+            {
+                if (i == dataList)
+                {
+                    cout << "Penyimpanan lagu penuh!\n";
+                }
+                else
+                {
+                    cout << "Masukkan nama lagu   : "; 
+                    cin.ignore();
+                    getline(cin, listLagu[i]);
+                    cout << "Masukkan durasi lagu : "; 
+                    cin >> durasiLagu[i];
+                    cout << "Masukkan genre lagu  : "; 
+                    cin.ignore();
+                    getline(cin, listGenre[i]);
+                    cin >> listGenre[i];
+                    insertEnd(listLagu[i], durasiLagu[i], listGenre[i]);
+                    i++;
+                }
+
                 getch();
                 break;
             }
-            case 2 : system("cls");{
-                if (isEmpty()){
-                    cout << "Maaf list kosong!\n";
+        case 2:
+            system("cls");
+            {
+                if (head == NULL)
+                {
+                    cout << "Maaf data masih kosong!\n";
                 } else {
-                    delData();
+                    deleteSong();
+                }
+                getch();
+                break;
+            }
+        case 3:
+            system("cls");
+            {
+                if (head == NULL){
+                    cout << "Maaf data lagu kosong!";
+                } else {
+                    showLagu();
                 }
                 
                 getch();
                 break;
             }
-            case 3 : system("cls");{
+        case 4:
+            system("cls");
+            {
                 return 0;
                 break;
             }
-            default : system("cls");{
+        default:
+            system("cls");
+            {
                 cout << "Maaf pilihan tidak ada!\n";
                 getch();
                 break;
             }
         }
-    } while (choose != 3);
-    
+    } while (choose != 4);
 
     cin.get();
     return 0;
--- pustaka.h
+++ pustaka.h
@@ -1,8 +1,11 @@
+#include <bits/stdc++.h>
 #include <iostream>
+#include <conio.h>
 
 using namespace std;
 
-struct lagu{
+struct lagu
+{
     string judul;
     int durasi;
     string genre;
@@ -13,7 +16,10 @@ struct lagu{
 lagu *head = NULL;
 lagu *tail = NULL;
 
-void insertEnd(string a, int b, string c){
+int i = 0;
+
+void insertEnd(string a, int b, string c)
+{
     lagu *baru;
     lagu *bantu;
     baru = new lagu;
@@ -23,29 +29,111 @@ void insertEnd(string a, int b, string c){
     baru->next = NULL;
     baru->prev = NULL;
 
-    if (head == NULL){
-        baru = head;
-        baru = tail;
+    if (head == NULL)
+    {
+        head = baru;
+        tail = baru;
         tail->next = head;
         head->prev = tail;
-    } else {
+        cout << "Data berhasil masuk!\n";
+    }
+    else
+    {
         tail->next = baru;
         baru->prev = tail;
         tail = baru;
         tail->next = head;
         head->prev = tail;
+        cout << "Data berhasil masuk!\n";
     }
 }
 
-void showLagu(){
+void deleteSong()
+{
+    lagu *bantu;
+    lagu *hapus;
+    string searchGenre;
+    bool found = false;
+    bantu = head;
+    cout << "Masukkan genre musik yang ingin dihapus : ";
+    cin.ignore();
+    getline(cin, searchGenre);
+    do
+    {
+        // MENGHAPUS JIKA DATA HANYA ADA 1
+        if (searchGenre == bantu->genre && head == tail)
+        {
+            hapus = head;
+            head = NULL;
+            tail = NULL;
+            found = true;
+            delete hapus;
+            i--;
+            break;
+        }
+
+        // MENGHAPUS DATA JIKA BERADA DI DEPAN
+        if (searchGenre == head->genre)
+        {
+            hapus = head;
+            head = head->next;
+            tail->next = head;
+            head->prev = tail;
+            found = true;
+            delete hapus;
+            bantu = bantu->next;
+            i--;
+            continue;
+        }
+
+        // MENGHAPUS JIKA DATA LEBIH DARI 1 DAN DI TENGAH
+        if (searchGenre == bantu->genre && searchGenre != head->genre)
+        {
+            hapus = bantu;
+            bantu->prev->next = bantu->next;
+            bantu->next->prev = bantu->prev;
+            found = true;
+            delete hapus;
+            bantu = bantu->next;
+            i--;
+            continue;
+        }
+
+        // MENGHAPUS DATA JIKA BERADA DI BELAKANG
+        if (searchGenre == tail->genre)
+        {
+            hapus = tail;
+            tail = tail->prev;
+            tail->next = head;
+            head->prev = tail;
+            found = true;
+            delete hapus;
+            bantu = bantu->next;
+            i--;
+            continue;
+        }
+    } while (bantu != NULL);
+
+    if (found == false)
+    {
+        cout << "Lagu tidak ditemukan!\n";
+    }
+    else
+    {
+        cout << "Genre " << searchGenre << " berhasil dihapus!\n";
+    }
+}
+
+void showLagu()
+{
     lagu *list;
     list = head;
     int i = 1;
     do
     {
         cout << i << ". " << list->judul << " | " << list->durasi
-        << " | " << list->genre << "\n";
+             << " | " << list->genre << "\n";
+        i++;
         list = list->next;
     } while (list != head);
-    
 }
